efb0cd38cac1e4d0ce9c04bf70ee5bc619afab0b
unknown <prakash@thehosteller.com>
2023-10-03 08:59:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:7000/api/jsondata")
+      .then((response) => {
+        if (response.status === 200) {
+          const jsonData = response.data;
+          setData(jsonData);
+        } else {
+          console.error("Failed to fetch data.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  console.log(data);
+
+  return <div className="App">appp</div>;
 }
 
 export default App;
